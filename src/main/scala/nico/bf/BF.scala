c209ad5d7e5b9d2d87a5275a2a2f19816884f964
KanameSato <kanamee.sato@gmail.com>
2018-06-10 20:26:55 +0900

    fix nico/bf/BF.scala

--- src/main/scala/nico/bf/BF.scala
+++ src/main/scala/nico/bf/BF.scala
@@ -1,13 +1,16 @@
 package nico.bf
 
-case class BF(mem: Vector[Byte], ptr: Int) {
+case class BF(mem: Vector[Byte], ptr: Int = 0) {
   def value: Byte = mem(ptr)
   def update(f: Byte => Byte): BF = copy(mem = mem.updated(ptr, f(this.value)))
 }
 
 object BF {
-  def parse(s: String): List[Operation] = parse(s.toList)
+  def sized(size: Int): BF = BF(Vector.fill(size)(0), 0)
 
+  def execute(seq: Seq[Operation]): BF = (sized(65535) /: seq){ (bf, op) => op.execute(bf) }
+
+  def parse(s: String): List[Operation] = parse(s.toList)
   def parse(list: List[Char], reverseLog: List[Operation] = Nil): List[Operation] = list match {
     case Nil     => reverseLog.reverse
     case (x::xs) if x == '+' => parse(xs, Incr  :: reverseLog)
@@ -25,7 +28,7 @@ object BF {
                                                                :(List[Char], List[Char]) = list match {
     case Nil => throw new Exception("']' not found")
     case (x::xs) if x == ']' && level == 0 => (reverseLog.reverse, xs)
-    case (x::xs) if x == ']'               => splitLevel(xs, level - 1, reverseLog)
-    case (x::xs)                           => splitLevel(xs, level, reverseLog)
+    case (x::xs) if x == ']'               => splitLevel(xs, level - 1, x :: reverseLog)
+    case (x::xs)                           => splitLevel(xs, level    , x :: reverseLog)
   }
 }

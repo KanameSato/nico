76ff00b0ee2d0560b626105b651798034f141dd9
KanameSato <kanamee.sato@gmail.com>
2018-06-10 20:36:55 +0900

    impl nico/Nico#execute

--- src/main/scala/nico/Nico.scala
+++ src/main/scala/nico/Nico.scala
@@ -1,10 +1,12 @@
 package nico
 
+import nico.bf._
+
 case class Nico(reverseOperations: List[Operation]) {
   def ^(op: Operation): Nico = copy(op :: reverseOperations)
 
   def toBFString: String = reverseOperations.reverse.map(_.bf).mkString
-  def execute: Unit = ???
+  def execute: Unit = BF.execute(BF.parse(toBFString))
 }
 
 object Nico {

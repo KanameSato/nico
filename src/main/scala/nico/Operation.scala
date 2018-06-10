8b93acdcef16e1469de9365883ce20df77178a38
KanameSato <kanamee.sato@gmail.com>
2018-06-10 19:07:38 +0900

    add nico/Operation.scala

---
+++ src/main/scala/nico/Operation.scala
@@ -0,0 +1,14 @@
+package nico
+
+sealed trait Operation {
+  val bf: String
+}
+
+object o extends Operation { val bf = "+" }
+object u extends Operation { val bf = "-" }
+object c extends Operation { val bf = ">" }
+object e extends Operation { val bf = "<" }
+object v extends Operation { val bf = "." }
+object a extends Operation { val bf = "," }
+object p extends Operation { val bf = "[" }
+object q extends Operation { val bf = "]" }

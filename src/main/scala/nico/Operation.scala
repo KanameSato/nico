598d2e7d336adf7fbcc3fa51d3d69c3c2d458141
KanameSato <kanamee.sato@gmail.com>
2018-06-10 20:57:20 +0900

    add Operation endline

--- src/main/scala/nico/Operation.scala
+++ src/main/scala/nico/Operation.scala
@@ -14,3 +14,4 @@ object p extends Operation { val bf = "[" }
 object q extends Operation { val bf = "]" }
 
 object end extends Operation { val bf = "" }
+object endline extends Operation { val bf = "" }

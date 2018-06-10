008a1bd072b69cfb1eed543485fc6e4e6f6d0a98
KanameSato <kanamee.sato@gmail.com>
2018-06-10 20:57:41 +0900

    impl helloworld

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,6 +3,11 @@ import nico.Nico._
 
 object Main extends App {
   (start
-    ^o^o^
+    ^c^o^o^o^o^o^o^o^o^o^p^e^o^o^o^o^o^o^o^o^c^u^q^e^v^c^o^o^o^o^o^o^ endline
+    ^o^p^e^o^o^o^o^c^u^q^e^o^v^o^o^o^o^o^o^o^v^v^o^o^o^v^p^u^q^c^o^o^ endline
+    ^o^o^o^o^o^o^p^e^o^o^o^o^c^u^q^e^v^c^o^o^o^o^o^o^o^o^o^o^o^p^e^o^ endline
+    ^o^o^o^o^c^u^q^e^v^c^o^o^o^o^o^o^o^o^p^e^o^o^o^c^u^q^e^v^o^o^o^v^ endline
+    ^u^u^u^u^u^u^v^u^u^u^u^u^u^u^u^v^p^u^q^c^o^o^o^o^o^o^o^o^p^e^o^o^ endline
+    ^o^o^c^u^q^e^o^v^p^u^q^o^o^o^o^o^o^o^o^o^o^v^
   end).execute
 }
